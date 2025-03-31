41df9b6278085f6f0da5d5230eaf1d2d8f2dc1f4
Egor <eesk98@gmail.com>
2025-03-31 21:47:31 +0700

    confg(): add linters, deps, ci

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+const perfectionist = require("eslint-plugin-perfectionist");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      eslintPluginPrettierRecommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    plugins: {
+      perfectionist,
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "no-empty-function": "off",
+      "@typescript-eslint/no-empty-function": "off",
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      eslintPluginPrettierRecommended,
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+);
